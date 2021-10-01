899daf2504ac6846a7a1c31376daa03eaa6d75e7
ryanop1 <91714108+ryanop1@users.noreply.github.com>
2021-10-01 12:43:27 +0530

    Add files via upload

---
+++ rectangle area .py
@@ -0,0 +1,66 @@
+'''import pyttsx3
+engine = pyttsx3.init()
+engine.say("Lambo Truck Vich Gedi Sidhi Hollywood Geet Desi Mundeya De Sune Bollywood  Music Di Wave Aa Na Bhalde Koi Fav Aa Gauna Vi Aunda Te Lyrics Ae")
+engine.runAndWait()
+'''
+
+
+
+
+
+
+
+
+
+
+
+# volume of cube
+v = int(input("enter side:"))
+k = v*v*v
+print(k)
+# area of circle
+'''
+j =3.14
+p = int(input("enter the radius"))
+k = j*p*p
+print(k)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+#square area
+# 
+''' 
+J = int(input("enter side : "))
+l = J*J
+print(l)
+'''
+
+
+#area of rectangle 
+'''L = int(input("enter the lenght"))
+B = int(input("enter the breadth"))
+a = L*B
+print(a)'''
+
+
+
+#area of Parallelogram
+'''
+o = int(input("enter the lenght"))
+l = int(input("enter the breadth"))
+j = o*l
+print(j)
+'''
+
+
